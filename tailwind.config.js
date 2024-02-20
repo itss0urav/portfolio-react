45438e644d0f47a535561758821ea29f39f549e5
itss0urav <sourav2021official@gmail.com>
2024-02-20 20:10:45 +0530

    ui update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ theme, addUtilities }) => {
+      const neonUtilities = {};
+      const colors = theme("colors");
+      for (const color in colors) {
+        if (typeof colors[color] === "object") {
+          const color1 = colors[color]["500"];
+          const color2 = colors[color]["700"];
+          neonUtilities[`.neon-${color}`] = {
+            boxShadow: `0 0 5px ${color1}, 0 0 20px ${color2}`,
+          };
+        }
+      }
+      addUtilities(neonUtilities);
+    }),
+  ],
+};
